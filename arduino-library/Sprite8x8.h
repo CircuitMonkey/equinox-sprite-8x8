215bc9b267470173012cd0af5fb46f1afcf209b7
Mark Koch <maehem@users.noreply.github.com>
2019-01-15 13:19:17 -0800

    Added interum changes

--- arduino-library/Sprite8x8.h
+++ arduino-library/Sprite8x8.h
@@ -1,42 +1,52 @@
+
 /*
+ * Sprite8x8 Arduino Library
+ * Version 0.0  Jan 2019
+ * Copyright 2019 Circuit Monkey
+ * MIT License
+ *
+ * Class API based upon:
  * MaxMatrix
  * Version 1.0 Feb 2013
  * Copyright 2013 Oscar Kin-Chung Au
+ * https://code.google.com/archive/p/arudino-maxmatrix-library/
  */
 
-#ifndef _MaxMatrix_H_
-#define _MaxMatrix_H_
+#ifndef _Sprite8x8_H_
+#define _Sprite8x8_H_
 
 #include "Arduino.h"
 
-#define max7219_reg_noop        0x00
-#define max7219_reg_digit0      0x01
-#define max7219_reg_digit1      0x02
-#define max7219_reg_digit2      0x03
-#define max7219_reg_digit3      0x04
-#define max7219_reg_digit4      0x05
-#define max7219_reg_digit5      0x06
-#define max7219_reg_digit6      0x07
-#define max7219_reg_digit7      0x08
-#define max7219_reg_decodeMode  0x09
-#define max7219_reg_intensity   0x0a
-#define max7219_reg_scanLimit   0x0b
-#define max7219_reg_shutdown    0x0c
-#define max7219_reg_displayTest 0x0f
+/*
+ #define max7219_reg_noop        0x00
+ #define max7219_reg_digit0      0x01
+ #define max7219_reg_digit1      0x02
+ #define max7219_reg_digit2      0x03
+ #define max7219_reg_digit3      0x04
+ #define max7219_reg_digit4      0x05
+ #define max7219_reg_digit5      0x06
+ #define max7219_reg_digit6      0x07
+ #define max7219_reg_digit7      0x08
+ #define max7219_reg_decodeMode  0x09
+ #define max7219_reg_intensity   0x0a
+ #define max7219_reg_scanLimit   0x0b
+ #define max7219_reg_shutdown    0x0c
+ #define max7219_reg_displayTest 0x0f
+ */
 
-class MaxMatrix
+class Sprite8x8
 {
-  private:
+private:
     byte data;
-    byte load;
-    byte clock;
+    //byte load;
+    //byte clock;
     byte num;
     byte buffer[80];
     
     void reload();
-	
-  public:
-    MaxMatrix(byte data, byte load, byte clock, byte num);
+    
+public:
+    Sprite8x8(byte data);
     
     void init();
     void clear();
