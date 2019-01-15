c5c24f0fe5e784aec94e250c69e119f5a52a9db1
Mark Koch <maehem@users.noreply.github.com>
2019-01-15 15:00:44 -0800

    Restructure src to match Arduino 1.5+

--- arduino-library/src/Sprite8x8.cpp
+++ arduino-library/src/Sprite8x8.cpp
@@ -13,15 +13,15 @@
 
 
 #include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
 #include "Sprite8x8.h"
 
-Sprite8x8::Sprite8x8(byte _data /*, byte _load, byte _clock, byte _num */)
-{
+Sprite8x8::Sprite8x8(byte _data /*, byte _load, byte _clock, byte _num */) {
     data = _data;
-    
+
     // Max in use, from older code.  ???
     num = 1;
-    
+
     //load = _load;
     //clock = _clock;
     //num = _num;
@@ -29,29 +29,29 @@ Sprite8x8::Sprite8x8(byte _data /*, byte _load, byte _clock, byte _num */)
         buffer[i] = 0;
 }
 
-void Sprite8x8::init()
-{
+void Sprite8x8::init() {
+
     pinMode(data,  OUTPUT); // Call AdaFruit NeoPixel init here.
-    
+
     //pinMode(clock, OUTPUT);
     //pinMode(load,  OUTPUT);
     //digitalWrite(clock, HIGH);
-    
+
     //setCommand(max7219_reg_scanLimit, 0x07);
     //setCommand(max7219_reg_decodeMode, 0x00);  // using an led matrix (not digits)
     //setCommand(max7219_reg_shutdown, 0x01);    // not in shutdown mode
     //setCommand(max7219_reg_displayTest, 0x00); // no display test
-    
+
     // empty registers, turn all LEDs off
     clear();
-    
+
     setIntensity(0x0f);    // the first 0x0f is the value you can set
 }
 
 void Sprite8x8::setIntensity(byte intensity)
 {
     //setCommand(max7219_reg_intensity, intensity);
-    
+
     // TODO: NeopPixel Set Brightness
 }
 
@@ -59,7 +59,7 @@ void Sprite8x8::clear()
 {
     for (int i=0; i<8; i++)
         setColumnAll(i,0);
-    
+
     for (int i=0; i<80; i++)
         buffer[i] = 0;
 }
@@ -67,7 +67,7 @@ void Sprite8x8::clear()
 // What does this do?
 void Sprite8x8::setCommand(byte command, byte value) {
     // AdaFruit NeoPixel foo.
-    
+
     /*
      digitalWrite(load, LOW);
      for (int i=0; i<num; i++)
@@ -77,14 +77,14 @@ void Sprite8x8::setCommand(byte command, byte value) {
      }
      digitalWrite(load, LOW);
      digitalWrite(load, HIGH);
-     
+
      */
 }
 
 
 void Sprite8x8::setColumn(byte col, byte value) {
     // AdaFruit NepPixel foo.
-    
+
     /*
      int n = col / 8;
      int c = col % 8;
@@ -104,14 +104,14 @@ void Sprite8x8::setColumn(byte col, byte value) {
      }
      digitalWrite(load, LOW);
      digitalWrite(load, HIGH);
-     
+
      */
     buffer[col] = value;
 }
 
 void Sprite8x8::setColumnAll(byte col, byte value) {
     // TODO: AdaFruit NeoPixel foo.
-    
+
     /*
      digitalWrite(load, LOW);
      for (int i=0; i<num; i++)
@@ -122,7 +122,7 @@ void Sprite8x8::setColumnAll(byte col, byte value) {
      }
      digitalWrite(load, LOW);
      digitalWrite(load, HIGH);
-     
+
      */
 }
 
@@ -130,11 +130,11 @@ void Sprite8x8::setDot(byte col, byte row, byte value) {
     // TODO AdaFruit Neopixel foo.
     /*
      bitWrite(buffer[col], row, value);
-     
+
      int n = col / 8;
      int c = col % 8;
      digitalWrite(load, LOW);
-     
+
      for (int i=0; i<num; i++) {
      if (i == n) {
      shiftOut(data, clock, MSBFIRST, c + 1);
@@ -144,18 +144,18 @@ void Sprite8x8::setDot(byte col, byte row, byte value) {
      shiftOut(data, clock, MSBFIRST, 0);
      }
      }
-     
+
      digitalWrite(load, LOW);
      digitalWrite(load, HIGH);
-     
-     
+
+
      */
 }
 
 void Sprite8x8::writeSprite(int x, int y, const byte* sprite) {
     int w = sprite[0];
     int h = sprite[1];
-    
+
     if (h == 8 && y == 0) {
         for (int i=0; i<w; i++) {
             int c = x + i;
@@ -179,13 +179,13 @@ void Sprite8x8::reload() {
      for (int i=0; i<8; i++) {
      int col = i;
      digitalWrite(load, LOW);
-     
+
      for (int j=0; j<num; j++) {
      shiftOut(data, clock, MSBFIRST, i + 1);
      shiftOut(data, clock, MSBFIRST, buffer[col]);
      col += 8;
      }
-     
+
      digitalWrite(load, LOW);
      digitalWrite(load, HIGH);
      }
@@ -199,12 +199,11 @@ void Sprite8x8::shiftLeft(bool rotate, bool fill_zero) {
         buffer[i] = buffer[i+1];
     if (rotate) buffer[num*8-1] = old;
     else if (fill_zero) buffer[num*8-1] = 0;
-    
+
     reload();
 }
 
-void Sprite8x8::shiftRight(bool rotate, bool fill_zero)
-{
+void Sprite8x8::shiftRight(bool rotate, bool fill_zero) {
     int last = num*8-1;
     byte old = buffer[last];
     int i;
@@ -212,12 +211,11 @@ void Sprite8x8::shiftRight(bool rotate, bool fill_zero)
         buffer[i] = buffer[i-1];
     if (rotate) buffer[0] = old;
     else if (fill_zero) buffer[0] = 0;
-    
+
     reload();
 }
 
-void Sprite8x8::shiftUp(bool rotate)
-{
+void Sprite8x8::shiftUp(bool rotate) {
     for (int i=0; i<num*8; i++)
     {
         bool b = buffer[i] & 1;
@@ -227,8 +225,7 @@ void Sprite8x8::shiftUp(bool rotate)
     reload();
 }
 
-void Sprite8x8::shiftDown(bool rotate)
-{
+void Sprite8x8::shiftDown(bool rotate) {
     for (int i=0; i<num*8; i++)
     {
         bool b = buffer[i] & 128;
@@ -237,5 +234,3 @@ void Sprite8x8::shiftDown(bool rotate)
     }
     reload();
 }
-
-
--- arduino-library/src/Sprite8x8.h
+++ arduino-library/src/Sprite8x8.h
@@ -17,37 +17,18 @@
 
 #include "Arduino.h"
 
-/*
- #define max7219_reg_noop        0x00
- #define max7219_reg_digit0      0x01
- #define max7219_reg_digit1      0x02
- #define max7219_reg_digit2      0x03
- #define max7219_reg_digit3      0x04
- #define max7219_reg_digit4      0x05
- #define max7219_reg_digit5      0x06
- #define max7219_reg_digit6      0x07
- #define max7219_reg_digit7      0x08
- #define max7219_reg_decodeMode  0x09
- #define max7219_reg_intensity   0x0a
- #define max7219_reg_scanLimit   0x0b
- #define max7219_reg_shutdown    0x0c
- #define max7219_reg_displayTest 0x0f
- */
-
 class Sprite8x8
 {
 private:
     byte data;
-    //byte load;
-    //byte clock;
     byte num;
     byte buffer[80];
-    
+
     void reload();
-    
+
 public:
     Sprite8x8(byte data);
-    
+
     void init();
     void clear();
     void setCommand(byte command, byte value);
@@ -56,7 +37,7 @@ public:
     void setColumnAll(byte col, byte value);
     void setDot(byte col, byte row, byte value);
     void writeSprite(int x, int y, const byte* sprite);
-    
+
     void shiftLeft(bool rotate = false, bool fill_zero = true);
     void shiftRight(bool rotate = false, bool fill_zero = true);
     void shiftUp(bool rotate = false);
